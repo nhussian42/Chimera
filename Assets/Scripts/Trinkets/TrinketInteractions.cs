557bbbddf3f5be48bdc43373158a7354b2f8c670
Braydon-Leamer <luminati.confirmed@yahoo.com>
2023-09-30 19:29:02 -0400

    scripts

---
+++ Assets/Scripts/Trinkets/TrinketInteractions.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:41bc9fa3efdd1e588b92ba31e7f6eaafd8828a0028d4335edfc66bca1e036efe
+size 423
---
+++ Assets/Scripts/Trinkets/TrinketMenu.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e58c0dfe46f70ecd9b4e63d0df961a998055e016bf3aa5be74f39f91f1a409e6
+size 655
