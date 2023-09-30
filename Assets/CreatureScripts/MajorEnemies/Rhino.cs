a2b5ed63ab4a1f2ad00fb94aca2aac474fc193d2
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-09-30 17:27:47 -0400

    rhino attack pattern adjusted, basic attack indicators added
    added some temporary attack indicators and adjusted the rhino attack pattern

--- Assets/CreatureScripts/MajorEnemies/Rhino.cs
+++ Assets/CreatureScripts/MajorEnemies/Rhino.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5a3d8bf7bf6bb4d9f8f391620b411e101868a86e1b9d01773fd58ed12db1de1
-size 2006
+oid sha256:f641053879f99ee442369e519fb3855661e858d6bc9ab37bda5897b5f1b6ac13
+size 2120
--- Assets/CreatureScripts/MinorEnemy/PirhanaDog.cs
+++ Assets/CreatureScripts/MinorEnemy/PirhanaDog.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee22b458096cb949817cf21e1479ab2e6402088959eb6d770829b23923c74176
-size 1189
+oid sha256:11c75be7120e83bf51ea31909844cc2159520ff21c4f6f3debe359d4c5cfd4d5
+size 1398
