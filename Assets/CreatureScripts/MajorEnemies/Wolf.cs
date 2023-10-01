ead62bd4694e7e20d0bbfcf5385cc29d4a2f0e6a
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-10-01 17:28:40 -0400

    finished wolf attack ai
    currently just charges, still need to add new attack indicator

--- Assets/CreatureScripts/MajorEnemies/Wolf.cs
+++ Assets/CreatureScripts/MajorEnemies/Wolf.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ccf4c6ec1c6c0f8bb8965982d14e725524c9c4eaa2ad7177fb98a23a9fca1a9d
-size 3117
+oid sha256:4afe510361eec925b7ed699339a47a5523639ad1479ed0eccc78acfd4dd099ac
+size 3597
