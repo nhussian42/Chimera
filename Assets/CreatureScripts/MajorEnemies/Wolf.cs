52abf3f00b1339afa2b3064f6d5e383d37e79f4b
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-10-01 18:55:53 -0400

    modified wolf attack

--- Assets/CreatureScripts/MajorEnemies/Wolf.cs
+++ Assets/CreatureScripts/MajorEnemies/Wolf.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4afe510361eec925b7ed699339a47a5523639ad1479ed0eccc78acfd4dd099ac
-size 3597
+oid sha256:22f9d05072678ffaa2016bb5ef77cd41945d14ea557e36983fa76cc92dd118b2
+size 3766
