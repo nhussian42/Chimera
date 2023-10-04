c1459370b8faa440606f8f0b0727cf5e829bf89e
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-10-03 20:59:51 -0400

    increased stopping distance on all navmesh agents, added new collider to rhino, adjusted wolf circling behavior

--- Assets/CreatureScripts/MajorEnemies/Wolf.cs
+++ Assets/CreatureScripts/MajorEnemies/Wolf.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22f9d05072678ffaa2016bb5ef77cd41945d14ea557e36983fa76cc92dd118b2
-size 3766
+oid sha256:28e2725923762717afdb7e325c88976918ed11ac00fdcdb4f85d2c7b02ee8f74
+size 3965
