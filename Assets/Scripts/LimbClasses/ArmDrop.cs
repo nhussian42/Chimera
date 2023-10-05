8a2c826ca1df6b956d2a825174ab2385299f568d
amguerrero2 <77288733+amgue-create@users.noreply.github.com>
2023-10-05 10:22:08 -0400

    Commit
    -Minor update to limb drop script

--- Assets/Scripts/LimbClasses/ArmDrop.cs
+++ Assets/Scripts/LimbClasses/ArmDrop.cs
@@ -7,10 +7,15 @@ public class ArmDrop : MonoBehaviour
     [SerializeField] private Arm ArmToDrop;
     public Arm armReference { get; private set; }
 
+    private float instanceCurrentHealth;
+
     private void OnEnable()
     {
         armReference = ArmToDrop;
     }
 
-
+    public void OverwriteHealth(float newHealth)
+    {
+        instanceCurrentHealth = newHealth;
+    }
 }
