c016e163942da6c6d7239f65df5515670be5bd57
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-10-09 11:08:10 -0400

    character anim changes

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e2e0abf0d6cec7c545d3bb14b85d191900cd4d8fbb477707d0a1920062a3703
-size 8616
+oid sha256:6650848b73c1e2520efe945a99124c969a29de9b0aa2f638395cf98a59782d0a
+size 8720
