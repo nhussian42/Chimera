f0fa95e2eb32d17c61e006779730b868b00d78ec
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-10-09 14:23:51 -0400

    Small update to health
    (BUG) swapping limbs works, but the health value breaks when you try to swap it
    (BUG)  player can transition between scenes, but the player's limb data does not persist because a new instance of the player is created

--- Assets/Scripts/AbstractClasses/Limb.cs
+++ Assets/Scripts/AbstractClasses/Limb.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22fbec50f9da7be482605b76cf684a8effa924e41636a83dc9a8a75b5504f37d
-size 1498
+oid sha256:ddcb04aff85cd1708b3d11eb30363c77c33a150d6c08d8dc09d92353029aae53
+size 1428
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ffaeb35fd9bd948984e97dbc6ce73a374d30fd531b23d3eb81dcafb671983c2
-size 9591
+oid sha256:38d7acea337222323d4537d21aec08c32824cc9c97e40518d8650ddf33509568
+size 10058
