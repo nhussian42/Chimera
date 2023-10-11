2b092d58c0782966cc61dc2beb617cfef79b45c6
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-10-10 20:33:46 -0400

    wolf anims added to behavior

--- Assets/Scripts/CreatureScripts/Creature.cs
+++ Assets/Scripts/CreatureScripts/Creature.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6f72ef525197464e2be4084ca1e67b85a784ac609e6d9d0decd62a0b70ec524
-size 1725
+oid sha256:d59eecea252a9c28709dbc9404719fae32cbe0cd385d7bbaaf43d8d2ea0c8823
+size 1828
