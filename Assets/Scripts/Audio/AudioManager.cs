90d60167233ba11d7023211b603d53fa0b99c15c
GabrielFernandez14 <gabrielfernandez14213@gmail.com>
2023-10-11 13:33:00 -0400

    Messing with Logarithmic/Linear Audio Sources

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -25,7 +25,7 @@ public class AudioManager : MonoBehaviour
 
     private void Start()
     {
-        // PlayMusic("DungeonMusic");
+        PlayMusic("DungeonMusic");
     }
 
     public void PlayMusic(string name) 
