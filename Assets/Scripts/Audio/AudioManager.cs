4adfadbc2f260070c88e2310d61921689baaeafd
GabrielFernandez14 <gabrielfernandez14213@gmail.com>
2023-10-11 12:57:13 -0400

    Begin Main Menu & AudioManager Implementation
    AudioManager still needs references throughout scripts

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:22ffd92e32435651edbb83ffa88d2668271742551e1f01518474ad8b7acfa717
+size 3103
---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:41e6a751c0ef5676efe66761080649e9249145a603ec7d433a5692738a5a71c5
+size 346
