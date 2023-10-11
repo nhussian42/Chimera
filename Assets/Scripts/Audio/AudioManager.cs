f793db560a637697bea09b4b72ad6e1ef0b7632c
GabrielFernandez14 <gabrielfernandez14213@gmail.com>
2023-10-11 13:33:00 -0400

    Messing with Logarithmic/Linear Audio Sources

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22ffd92e32435651edbb83ffa88d2668271742551e1f01518474ad8b7acfa717
-size 3103
+oid sha256:4e12b5a492f995e4d54198a6d4fa19da2abf89c399e0d8f07eaed5473c4fcd2d
+size 3100
