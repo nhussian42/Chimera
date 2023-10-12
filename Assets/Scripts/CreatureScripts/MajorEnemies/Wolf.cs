327b631eba836359c1eb53e3de3c43a7d92fe33f
GabrielFernandez14 <gabrielfernandez14213@gmail.com>
2023-10-11 20:42:21 -0400

    Implemented Basic Audio
    AudioManager Prefab Updated, will AudioManager in ChimeraPOC hierarchy will need to be deleted assuming MainMenu is working correctly.

--- Assets/Scripts/CreatureScripts/MajorEnemies/Wolf.cs
+++ Assets/Scripts/CreatureScripts/MajorEnemies/Wolf.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d2b17c8ff2d63d0a4384194467b736b6bf90ff10db8d6918e688271fb68ae36
-size 4713
+oid sha256:9ca69e79e0d90355211cd8bc0458d7c787b2dc80a7b02a0d22b0a285aba72735
+size 4967
--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41e6a751c0ef5676efe66761080649e9249145a603ec7d433a5692738a5a71c5
-size 346
+oid sha256:bf87b5f90ec6d805f6cb6f8c50a97bb101fecc9406a8a0047cf0776bf45ae3a3
+size 403
