1f24f190ae0283fb02340511fdf5c7408aa2748f
GabrielFernandez14 <gabrielfernandez14213@gmail.com>
2023-10-11 20:48:05 -0400

    Level Music Implement
    Added to Main Menu script dungeon music.

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -8,6 +8,8 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         AudioManager.Instance.PlayMenuSFX("SelectPlay");
+        AudioManager.Instance.musicSource.Stop();
+        AudioManager.Instance.PlayMusic("DungeonMusic");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void QuitGame()
