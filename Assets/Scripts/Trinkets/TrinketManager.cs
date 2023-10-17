e7bc038bc0208152eda03fee12f3580d9e2d4853
Braydon-Leamer <luminati.confirmed@yahoo.com>
2023-10-17 16:54:38 -0400

    cleaned scripts

--- Assets/Scripts/Trinkets/TrinketManager.cs
+++ Assets/Scripts/Trinkets/TrinketManager.cs
@@ -10,51 +10,59 @@ public class TrinketManager : MonoBehaviour
     private bool hyenaJaw = false;
     private bool feedingFrenzy = false;
 
+    private Arm LeftArm;
+    private Arm RightArm;
 
     private float coreHealth;
-    private bool HasRun = false;
+    private bool HJHasRun = false;
 
     // Start is called before the first frame update
+
+    public void Start()
+    {
+        LeftArm = PlayerController.Instance.currentLeftArm;
+        RightArm = PlayerController.Instance.currentRightArm;
+    }
 
     public void Update()
     {
         coreHealth = PlayerController.Instance.CoreHealth;
         if (hyenaJaw) // Actively updates damage based on health (WIP) - Doesn't reset on healing
         {
-            if((coreHealth < 50) && (coreHealth > 25) && HasRun == false)
+            if((coreHealth < 50) && (coreHealth > 25) && HJHasRun == false)
             {
-                PlayerController.Instance.currentLeftArm.UpdateAttackDamage(10);
-                PlayerController.Instance.currentRightArm.UpdateAttackDamage(10);
-                HasRun = true;
+                LeftArm.UpdateAttackDamage(10);
+                RightArm.UpdateAttackDamage(10);
+                HJHasRun = true;
             }
-            if(coreHealth < 25 && HasRun == true) 
+            if(coreHealth < 25 && HJHasRun == true) 
             {
-                PlayerController.Instance.currentLeftArm.UpdateAttackDamage(10);
-                PlayerController.Instance.currentRightArm.UpdateAttackDamage(10);
-                HasRun = false;
+                LeftArm.UpdateAttackDamage(10);
+                RightArm.UpdateAttackDamage(10);
+                HJHasRun = false;
             }
         }
     }
 
     public void LizardClaw() //Increases Attack Damage
     {
-        PlayerController.Instance.currentLeftArm.UpdateAttackDamage(5);
-        PlayerController.Instance.currentRightArm.UpdateAttackDamage(5);
+        LeftArm.UpdateAttackDamage(5);
+        RightArm.UpdateAttackDamage(5);
     }
 
     public void BirdTalon() //Increases Attack Speed
     {
-        PlayerController.Instance.currentLeftArm.UpdateAttackSpeed(0.05f);
-        PlayerController.Instance.currentRightArm.UpdateAttackSpeed(0.05f);
+        LeftArm.UpdateAttackSpeed(0.05f);
+        RightArm.UpdateAttackSpeed(0.05f);
     }
 
     public void TuftOfFur() //Increases Max Health (WIP for core)
     {
 
-        PlayerController.Instance.currentLeftArm.UpdateMaxHealth(5);
-        PlayerController.Instance.currentRightArm.UpdateMaxHealth(5);
+        LeftArm.UpdateMaxHealth(5);
+        RightArm.UpdateMaxHealth(5);
         //PlayerController.Instance.UpdateCoreHealth(5);
-        PlayerController.Instance.currentRightArm.DebugLog();
+        RightArm.DebugLog();
     }
 
     public void BearClaw() //Increases damage on every 3rd hit (WIP)
@@ -69,8 +77,8 @@ public class TrinketManager : MonoBehaviour
 
     public void PlumpMushroom() //Heals Player after room clear (WIP)
     {
-        PlayerController.Instance.currentLeftArm.UpdateCurrentHealth(5);
-        PlayerController.Instance.currentRightArm.UpdateCurrentHealth(5);
+        LeftArm.UpdateCurrentHealth(5);
+        RightArm.UpdateCurrentHealth(5);
         PlayerController.Instance.UpdateCoreHealth(5);
         
     }
