1f48653b3b5ce2449bfc50228e65ac66d5009652
amguerrero2 <77288733+amgue-create@users.noreply.github.com>
2023-10-19 11:39:14 -0400

    Commit
    - Proto Solution for same arm type switching
    -Better methods for swappin limbs

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:659af8087ba9dc90f903e1a60306d5b3bfe2c075ed8da5f4a6f02932c6e235af
-size 19728
+oid sha256:ba5600d9f979faf65d70e350a39f3856c2fa0a83347ee1388e537cf6987bef7b
+size 20026
