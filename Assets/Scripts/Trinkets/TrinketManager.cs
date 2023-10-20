cf8c501c2df3e40d03154610cfafcb69a8904991
Braydon-Leamer <luminati.confirmed@yahoo.com>
2023-10-20 13:49:19 -0400

    Merged Dev fixed

--- Assets/Scripts/Trinkets/TrinketManager.cs
+++ Assets/Scripts/Trinkets/TrinketManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f648e6ea871c7f0364493af83f283e75eb550d7cdfd40090a4572862e2a8bd9c
-size 5271
+oid sha256:d87355f29bb031ebdbcb16304b8714ebb0ec58260bd176b1d6d7fdbbc3c7a750
+size 5272
--- Assets/Scripts/UI/PlayerStatsUITest.cs
+++ Assets/Scripts/UI/PlayerStatsUITest.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad4c0825a3626f30d9dbf72befc8c8ef4626ffb0989ae8e44edcc374f557fdbe
-size 2016
+oid sha256:4fad99c151adf7fdd3dedf08d9ece7f2073065b4198d60183cf64bce38e48b7b
+size 2067
