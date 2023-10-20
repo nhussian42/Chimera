caa3b9de627c0514bfa101f285441c960dcea8df
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-10-19 20:12:25 -0400

    updated croc behavior
    also rebaked some navmesh

--- Assets/Scripts/CreatureScripts/MajorEnemies/Crocodile.cs
+++ Assets/Scripts/CreatureScripts/MajorEnemies/Crocodile.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2fc5afa34de547864c8ef0862480346a6d76ac2c3a13e1f241be98722ec2b0e0
-size 3558
+oid sha256:58cced47a026806322b6eb0e94a86410fc80c313211f30bd858a607ecec65c8c
+size 5177
--- Assets/Scripts/CreatureScripts/NotBossAI.cs
+++ Assets/Scripts/CreatureScripts/NotBossAI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dcd8176f5ca52d18278a277d6f2184483ed19cf47c9a5a1c7d63d776299702ed
-size 1878
+oid sha256:00e5b38d6b052617b2f0943f630cc79b3ae7549d78892ce603f6fc5a8b6d8420
+size 1875
