9710c6b5c22626a9724f259b8a02230f9dbda9c3
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-10-21 16:29:22 -0400

    last file organization im doing this weekend

--- Assets/Scripts/Trinkets/TrinketManager.cs
+++ Assets/Scripts/Trinkets/TrinketManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5bf6dce9392c66a7bc623be6b3211d5cf098e2cfc4cfed9e3b837fa12f58e68
-size 5715
+oid sha256:6d3e8d0303586c266b23798373f6e30c8ac58fd7d19fee0af6321a0026be59bc
+size 5721
