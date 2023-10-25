b0970be3aefdf28e1759dc83c002d198ea825625
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-10-25 10:10:24 -0400

    save manager fix through singleton

--- Assets/Scripts/Abstract Classes/Singleton.cs
+++ Assets/Scripts/Abstract Classes/Singleton.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:67abe0d00594a47ac8d655fdde0bb443afe61ee9bb2c2951311ae4bfbdb22bd3
-size 689
+oid sha256:3ed5cf47940d9b855cca19370d34746ce9ea314926696cf771f3c64adc73e9fe
+size 713
