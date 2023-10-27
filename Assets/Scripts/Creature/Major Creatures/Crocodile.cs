ce95a2364170cd42570a646531e5a28f752948e1
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-10-27 18:57:31 -0400

    basic gecko behavior

--- Assets/Scripts/Creature/Major Creatures/Crocodile.cs
+++ Assets/Scripts/Creature/Major Creatures/Crocodile.cs
@@ -48,7 +48,7 @@ public class Crocodile : NotBossAI
         agent.stoppingDistance = 7;
         yield return new WaitUntil(() => agent.remainingDistance < 10f);
     
-        rb.AddForce(gameObject.transform.forward * chargeSpeed, ForceMode.Impulse);
+        rb.AddForce((gameObject.transform.forward * agent.remainingDistance)/2, ForceMode.Impulse);
         animator.SetBool("Charge", true);
         attackCollider.enabled = true;
         yield return new WaitForSeconds(0.5f);
