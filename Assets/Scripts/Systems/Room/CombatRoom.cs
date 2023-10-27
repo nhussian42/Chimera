23e12890c3c1cd5cd82ac47b63f31666a5c7ba11
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-10-27 01:17:22 -0400

    Creature room updates, somewhat player movement bug fix

--- Assets/Scripts/Systems/Room/CombatRoom.cs
+++ Assets/Scripts/Systems/Room/CombatRoom.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aff07151f6da574a19107ddaf060db0aeca12f7683354a0f8cc28885bbc5c74b
-size 1365
+oid sha256:b9e4a486d6d6bf85bdac309cb57d820e348c4c7e18b2b2b1b4c102d2e4d8d4e3
+size 1330
--- Assets/Scripts/Systems/Room/Spawner.cs
+++ Assets/Scripts/Systems/Room/Spawner.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee9f1b7807251f5b7a0242b0d1ec88e2c1478dccc094126eb0f1132b8e45a4e8
+oid sha256:ec1e5a712f75a6fe4c87e01843cdacc1bafdace789b5627a58cea83cf306c2e9
 size 2073
