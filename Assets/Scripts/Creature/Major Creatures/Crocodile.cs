f31892b38193aba97c07b05c89b9c9d6b8b30cb3
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-10-27 18:57:31 -0400

    basic gecko behavior

--- Assets/Scripts/Creature/Major Creatures/Crocodile.cs
+++ Assets/Scripts/Creature/Major Creatures/Crocodile.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a475a60df53af975a341a23cb4ac3f6893cfa7b62f946df03cc4c1d08228dcd
-size 4479
+oid sha256:e5cd8d1e22130aeb726bdc413a5fd6e7875dcd88d8124b0e822e5d9c98736bf5
+size 4495
---
+++ Assets/Scripts/Creature/Major Creatures/Gecko.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:49a01e4126fa92e0303b4fac1a9439333b96a9a048ab768b65e40b3c3696ba3e
+size 4462
