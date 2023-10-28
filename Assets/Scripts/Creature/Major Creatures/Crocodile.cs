453ce077ea3c8fb5df7bee21a2d8fbf8d619d086
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-10-28 03:56:50 -0400

    updated base offset on all creatures

--- Assets/Scripts/Creature/Major Creatures/Crocodile.cs
+++ Assets/Scripts/Creature/Major Creatures/Crocodile.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5cd8d1e22130aeb726bdc413a5fd6e7875dcd88d8124b0e822e5d9c98736bf5
-size 4495
+oid sha256:0b1439d584ca23dd5cc7f3a4f239d80d2139fbddccecfc01eff2ffbb0e608534
+size 4493
