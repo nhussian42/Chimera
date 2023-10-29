5dc3ac501c5c6b9523b36d60bf7e5e77f0b884c7
JackEason19 <jack.eason19@gmail.com>
2023-10-29 13:40:34 -0400

    Trinket Slot Initialization Fix

--- Assets/Scripts/UI/Screen Space/EMScript.cs
+++ Assets/Scripts/UI/Screen Space/EMScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9cd164e3ff3394cc9dd68d14fd525b222b675588dc4efe3178172e536730b034
-size 654
+oid sha256:858d0fd0ebebc2f203a818e557a31bcb5bfed4ede4807b25a059c20d7c62d89c
+size 658
