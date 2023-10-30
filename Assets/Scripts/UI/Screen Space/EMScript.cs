526fe8ef5ee55a8235fe88a14905914c85573155
JackEason19 <jack.eason19@gmail.com>
2023-10-30 12:32:39 -0400

    Tinket On Click Events

--- Assets/Scripts/UI/Screen Space/EMScript.cs
+++ Assets/Scripts/UI/Screen Space/EMScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:858d0fd0ebebc2f203a818e557a31bcb5bfed4ede4807b25a059c20d7c62d89c
-size 658
+oid sha256:e26b2c203a25c71faf84a64b8daaa13b5c72132495e61be33cea4dfc7c303914
+size 867
--- Assets/Scripts/UI/Screen Space/TrinketInvSlot.cs
+++ Assets/Scripts/UI/Screen Space/TrinketInvSlot.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7654481a737095bbc8570da4d8fcf5d0475968d3dcb168517ba4f852eeb479b3
-size 133
+oid sha256:bc7041257cc5d44c32f76de8870b1a806ffee0bceee30598900c5c757bdcf447
+size 1406
