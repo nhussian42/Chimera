cdd5170c5708023ee926ff65350a2c29a70cdefa
amguerrero2 <77288733+amgue-create@users.noreply.github.com>
2023-10-30 10:16:50 -0400

    Commit
    Updated compound trinket

--- Assets/Scripts/Systems/Scriptable Objects/Trinkets/CompoundTrinket.cs
+++ Assets/Scripts/Systems/Scriptable Objects/Trinkets/CompoundTrinket.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19d2d84a24156e602fded6d17083e588af15bd602277ff81649afd543929583f
-size 1047
+oid sha256:5e30c0f6754562ed213796e7ddc4ec4c5db2ef2a735c2a329e87ae1337d71070
+size 1079
