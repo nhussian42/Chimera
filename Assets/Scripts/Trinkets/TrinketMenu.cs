1c894f68309d612279fca934e323b6c574042392
Nick <56888291+nhussian42@users.noreply.github.com>
2023-11-02 17:01:13 -0400

    Body Shop Update

--- Assets/Scripts/Trinkets/TrinketMenu.cs
+++ Assets/Scripts/Trinkets/TrinketMenu.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,9 +9,12 @@ public class TrinketMenu : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public Button Option1;
-    public Button Option2;
-    public Button Option3;
+    public List<Button> OptionButtons;
+    public List<TextMeshProUGUI> OptionTexts;
+
+    public List<int> TotalTrinkets;
+    public List<int> SelectedTrinket;
+    public List<int> RandomInts;
 
     public bool active = false;
 
@@ -17,6 +22,122 @@ public class TrinketMenu : MonoBehaviour
     public TrinketManager TrinketManager;
     
     public GameObject self;
+
+    private void Start()
+    {
+        SetupButtons();
+    }
+
+    private void SetupButtons()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            RandomInts[i] = TotalTrinkets[Random.Range(0, TotalTrinkets.Count)];
+            SelectedTrinket[i] = RandomInts[i];
+            TotalTrinkets.Remove(RandomInts[i]);
+
+
+ 
+
+            Debug.Log("Random Trinket #" + SelectedTrinket[i]);
+
+            switch (SelectedTrinket[i])
+            {
+                case 0:
+                    {
+                        OptionTexts[i].text = $"Lizard Claw";
+                        break;
+                    }
+                case 1:
+                    {
+                        OptionTexts[i].text = $"Bird Talon";
+                        break;
+                    }
+                case 2:
+                    {
+                        OptionTexts[i].text = $"Tuft of Fur";
+                        break;
+                    }
+                case 3:
+                    {
+                        OptionTexts[i].text = $"Feeding Frenzy";
+                        break;
+                    }
+                case 4:
+                    {
+                        OptionTexts[i].text = $"Plump Mushroom";
+                        break;
+                    }
+                case 5:
+                    {
+                        OptionTexts[i].text = $"Mule's Kick";
+                        break;
+                    }
+                case 6:
+                    {
+                        OptionTexts[i].text = $"Hyena Jaw";
+                        break;
+                    }
+                case 7:
+                    {
+                        OptionTexts[i].text = $"Scavenger";
+                        break;
+                    }
+            }
+        }
+    }
+
+    public void TrinketChoice(int i)
+    {
+        Debug.Log(SelectedTrinket[i]);
+
+        switch (SelectedTrinket[i])
+        {
+            case 0:
+                {
+                    TrinketManager.LizardClaw();
+                    break;
+                }
+            case 1:
+                {
+                    TrinketManager.BirdTalon();
+                    break;
+                }
+            case 2:
+                {
+                    TrinketManager.TuftOfFur();
+                    break;
+                }
+            case 3:
+                {
+                    TrinketManager.EnableFeedingFrenzy(); 
+                    break;
+                }
+            case 4:
+                {
+                    TrinketManager.PlumpMushroom();
+                    break;
+                }
+            case 5:
+                {
+                    TrinketManager.MulesKick();
+                    break;
+                }
+            case 6:
+                {
+                    TrinketManager.HyenaJaw();
+                    break;
+                }
+            case 7:
+                {
+                    TrinketManager.Scavenger();
+                    break;
+                }
+        }
+    }
+
+
+
     public void CloseMenu()
     {
         DestroyImmediate(self, true);
