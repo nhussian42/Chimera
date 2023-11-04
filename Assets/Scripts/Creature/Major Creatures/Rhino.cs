492f7bab34563d3ba2b7fbc15f1b81674cac2f80
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-11-04 18:21:54 -0400

    changed player prefab, updated rhino

--- Assets/Scripts/Creature/Major Creatures/Rhino.cs
+++ Assets/Scripts/Creature/Major Creatures/Rhino.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90079482b0053026229ffdcdfcb2be6518ded524cd7e85a106a8e0d4a3b5afaf
+oid sha256:a0cd74b2395d3e1e13696b9eb8eb9e527f3af3433260af0132dd9ce119d86278
 size 3445
