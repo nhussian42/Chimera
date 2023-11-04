cae5b930606cefb61b75173071750618dd288214
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-11-03 20:31:57 -0400

    fixed croc knockback

--- Assets/Scripts/Creature/Major Creatures/Crocodile.cs
+++ Assets/Scripts/Creature/Major Creatures/Crocodile.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7b67bfad794f8f8f1b3242ea5cb53a9ffee036abbc99fbcd743bb554d04a24a
-size 5350
+oid sha256:b0e1bdb7facfd6fe63ff7fdd32910c629079f1fa229f866396d78e0372b9a68c
+size 5410
