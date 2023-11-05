8212961cf02cd5b3731222bf7a69c73a29c986d1
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-11-05 00:24:07 -0400

    Updated rhino behavior, added knockback variable to all creatures

--- Assets/Scripts/Creature/Major Creatures/Rhino.cs
+++ Assets/Scripts/Creature/Major Creatures/Rhino.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:967a8fb77300014ce47cd88ddfcc123a90e30fff96b911b1ac13f694a882b869
-size 5706
+oid sha256:4916944281b35911209da832ba5065ace844f2bbc18645a3bb589f25ace23602
+size 6068
