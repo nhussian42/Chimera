73d7e8bf87c5c8dea571f5453490db0f0b3d5081
Braydon-Leamer <luminati.confirmed@yahoo.com>
2023-11-06 01:58:42 -0500

    Updated Trinket Menu

--- Assets/Scripts/Trinkets/NewTrinketManager.cs
+++ Assets/Scripts/Trinkets/NewTrinketManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df9e8d1dab6aa35843bb9b69105b2024f7068d31cc9509a86ea67cd95c78cb73
-size 2455
+oid sha256:a56c02a93984811133479b9b52db9fcbddf8a511c2266100838b35d3bae84c36
+size 2778
