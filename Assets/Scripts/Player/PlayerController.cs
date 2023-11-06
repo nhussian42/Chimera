5606e407930ddbc32eb7b889e4228a5f93eee28e
Nick <56888291+nhussian42@users.noreply.github.com>
2023-11-06 15:44:31 -0500

    example scene update with new player

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0256652f5f24cb0b641a6d937c9a59a4d689edb8b1ae68bf4a87ab681f559b06
-size 26507
+oid sha256:d8f167ef264ce0ff7db3b94c9912fa8d4230605c59126af5de219cf7606cd690
+size 26437
