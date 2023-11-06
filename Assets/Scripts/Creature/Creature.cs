7d0b2b129c448627c88dbf55c877ffcc8cfa0c79
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-11-06 10:57:36 -0500

    Limb drops from creatures & updated player prefab

--- Assets/Scripts/Creature/Creature.cs
+++ Assets/Scripts/Creature/Creature.cs
@@ -36,7 +36,7 @@ public abstract class Creature : MonoBehaviour
 
     [SerializeField] private CreatureType creatureType;
 
-    //[SerializeField] List<GameObject> drops;
+    // [SerializeField] List<GameObject> drops;
 
     protected Animator animator;
     protected NavMeshAgent agent;
@@ -51,11 +51,13 @@ public abstract class Creature : MonoBehaviour
     public void OnEnable()
     {
         DebugControls.DamageAllCreatures += TakeDamage;
+        FloorManager.AllCreaturesDefeated += DestroyCreature;
     }
 
     public void OnDisable()
     {
         DebugControls.DamageAllCreatures -= TakeDamage;
+        FloorManager.AllCreaturesDefeated -= DestroyCreature;
     }
 
     private void SpawnDrop(GameObject objToSpawn)
@@ -100,11 +102,23 @@ public abstract class Creature : MonoBehaviour
         Rigidbody rb = GetComponent<Rigidbody>();
         //rb.isKinematic = true;
         alive = false;
+
         CreatureManager.AnyCreatureDied?.Invoke();
-        Destroy(this.gameObject, 1.5f);
+        if (creatureType == CreatureType.Minor)
+            DestroyCreature();
+        
+        healthbar.gameObject.SetActive(false);
+
+        //Destroy(this.gameObject, 1.5f);
         StopAllCoroutines();
         //Something happens
         //Death
+    }
+
+    private void DestroyCreature()
+    {
+        // play the dissolve shader
+        Destroy(this.gameObject, 1.5f);
     }
 
     public void Knockback(Vector3 knockbackDir, float knockbackForce, float knockbackDuration)
@@ -144,12 +158,4 @@ public abstract class Creature : MonoBehaviour
         }
         yield return null;
     }
-
-    // protected void SpawnDrop()
-    // {
-    //    foreach(GameObject drop in drops)
-    //     {
-    //         Instantiate(drop, transform.position, Quaternion.identity);
-    //     }
-    // }
 }
