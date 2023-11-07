eab61e0c976fcd8ddc3bcfaa8b93846ac885d5c0
GabrielFernandez14 <gabrielfernandez14213@gmail.com>
2023-11-07 13:38:48 -0500

    Setup Main&Pause Menu Volume Sliders

--- Assets/Scripts/UI/Screen Space/SettingsController.cs
+++ Assets/Scripts/UI/Screen Space/SettingsController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62f36c5b71c4559676d920c6f2cc9f6638b9e669ce11533171d7ce80ecebcc00
-size 1800
+oid sha256:56ec217389a47efe6af65058295b180c2cab33714498df05ae31cb71a2a23389
+size 2007
