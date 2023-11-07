4022b622341919fd064ae44d50451906aef88fd4
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-11-06 23:50:37 -0500

    new trinket menu setup in dev

--- Assets/Scripts/Trinkets/NewTrinketManager.cs
+++ Assets/Scripts/Trinkets/NewTrinketManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:737d714ee77ef896b0e37d71e9f21fa5f5f63c334bfd3d69fdcd4034a0b6154d
-size 2938
+oid sha256:bfa44cfe6280c9d2464f5e2ca1cafd1695ead0cae818780a7562343f24e57a76
+size 2947
