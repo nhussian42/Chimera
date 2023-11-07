1d5b525586302d6b1e654685ac7e17e18bdcdcde
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-11-06 19:20:48 -0500

    attacks deal damage fix

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a9fb65671e903c7eb2d87379ab736c4484b2bbd0abee04a985db829ad6d10c3
-size 27110
+oid sha256:32bb4e8cc1507c868280eb83d624025de26e5140ead2eb6a6fd48db801178d84
+size 27153
