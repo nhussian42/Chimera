32628d91a09e13a56eb5e8704d4621f6e4a51b94
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-11-08 23:46:47 -0500

    rhino animation event

--- Assets/Scripts/Creature/BossAI.cs
+++ Assets/Scripts/Creature/BossAI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8150595adf2a103c26549f9d31420688621469fb0774ab45e433da98687567c
-size 974
+oid sha256:f50c5f90772d0e3419f182ca1d2bddf2e0f1e03dd92511ef17d1369330ef447c
+size 965
