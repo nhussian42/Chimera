22c2a258615e3c43fca848614d3cb7ba306dc206
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-11-09 13:44:01 -0500

    started working on projectile attack

--- Assets/Scripts/Creature/Grolfino/Grolfino.cs
+++ Assets/Scripts/Creature/Grolfino/Grolfino.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6c915dda6e18602c6aed3b5ae6813088385863a6d1bbcb8976157011a7524b1
-size 3091
+oid sha256:4656326849496f78a226b68ecedaf311740fda9906cceda5a9fb8569ecccb694
+size 3859
