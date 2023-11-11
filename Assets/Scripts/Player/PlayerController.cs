b8d7da5e3eaae9f92781c13b2bcaff8f5d15b2d1
Braydon-Leamer <luminati.confirmed@yahoo.com>
2023-11-11 03:11:31 -0500

    repair player controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:70fad45d020bb631a9f61b56e21e7a36e545acfa00ef3663f14a43a7bb0426da
-size 34842
+oid sha256:c58b82d8ad6b1d168cd48a238f1d2eb9823e57dbb38cf0ad16bb95236572cb0e
+size 34729
