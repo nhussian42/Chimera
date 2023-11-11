ab2e1efb3d548e091f02cf92c9d82315262ce04b
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-11-11 15:13:26 -0500

    grolfino attacks updated

--- Assets/Scripts/Creature/BossAI.cs
+++ Assets/Scripts/Creature/BossAI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f50c5f90772d0e3419f182ca1d2bddf2e0f1e03dd92511ef17d1369330ef447c
-size 965
+oid sha256:7ea12b17be7292a4a6e9749f9f5c4b023b7d62be01d3afe9ef49d2baa578beae
+size 989
--- Assets/Scripts/Creature/Grolfino/Grolfino.cs
+++ Assets/Scripts/Creature/Grolfino/Grolfino.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c7e11fe13b1bf7484e31a2d55676b36b5e8ee6d8657f944d08f783c58c3527a
-size 4873
+oid sha256:599ecabd7e4c091429602f291b3bf2520e7372b41a87e5a1c4a02227bf88613f
+size 5333
