12ebcf09608a407699eac28e2c17f3f2e82f0e5d
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-11-12 17:26:56 -0500

    attack update 2

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81e13977d4b6f41522793b919e75393fe37b10cbb11bd8b3a0a1d61577d9ab59
-size 30689
+oid sha256:d843fd9edfb2295314662557cdc74849258e6c8f6dc4e8a80eefbc10549c7e08
+size 31311
