81633f6502045b51509cac39e0c3e4d47540c478
Braydon-Leamer <luminati.confirmed@yahoo.com>
2023-11-12 02:30:14 -0500

    updated equip menu

--- Assets/Scripts/UI/Screen Space/EMScript.cs
+++ Assets/Scripts/UI/Screen Space/EMScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1f0c5ca90fd5bc7034ae38f108e3ed65b3ec05f0336718b62d4a399708dcb6c
-size 3429
+oid sha256:d04fa06a5c0ca37ebb8ad743a0c97217a113ff21ba87e5fc7287c44a62ca395a
+size 3513
--- Assets/Scripts/UI/Screen Space/PlayerInvSlot.cs
+++ Assets/Scripts/UI/Screen Space/PlayerInvSlot.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52c1098e6aa89631a396dd596c4ca1a63ea858f9cf4816dcb0a228695fdc7a65
-size 2177
+oid sha256:bf6117a58d2f0c9c47e90fc4d0417e71fe7d230251a848bb4161f6f50e44129b
+size 2920
