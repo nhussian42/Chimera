2db171d2794399717402f93950e171b32e2d4d8f
Braydon-Leamer <luminati.confirmed@yahoo.com>
2023-11-13 02:44:42 -0500

    Updated EquipMenu Again

--- Assets/Scripts/Abstract Classes/Limb.cs
+++ Assets/Scripts/Abstract Classes/Limb.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96585c764c268e4cd337fe14220c1027d593c9ceaa073e8f216fe928ee041d1c
-size 1484
+oid sha256:fcd0bd7c119e3e3275d277d510aadd7e3419994b94681e95c23cba5a38635c7e
+size 1625
--- Assets/Scripts/UI/Screen Space/PlayerInvSlot.cs
+++ Assets/Scripts/UI/Screen Space/PlayerInvSlot.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf6117a58d2f0c9c47e90fc4d0417e71fe7d230251a848bb4161f6f50e44129b
-size 2920
+oid sha256:77d739c4aac54242bb1a12c6ecc22f74601f1ade641cdf4ae39f0b55368bf09e
+size 3612
