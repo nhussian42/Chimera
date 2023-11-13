69836f08f75f114e5f43cc7160437ff332cb0d5f
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-11-12 19:13:05 -0500

    rhino arm added, attack speeds adjusted

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d843fd9edfb2295314662557cdc74849258e6c8f6dc4e8a80eefbc10549c7e08
+oid sha256:5e6980e868eb1aa874b252d548c3b57374f6bf1cd074873ef3df763d7dd9e4f0
 size 31311
