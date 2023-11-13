1a893399db54e0323644719cf6c2070ac4896796
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-11-13 00:06:48 -0500

    Boss performs each attack once and then resets

--- Assets/Scripts/Creature/Grolfino/Grolfino.cs
+++ Assets/Scripts/Creature/Grolfino/Grolfino.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:857147c0b4dd31490c417ce50752437555ece07719e503bf0865be10f2ee9c9c
-size 8120
+oid sha256:aa7ee37c178574cfdc01071bf5b291c1fba98bee71cbb0b4b2caab930f5ae017
+size 8797
