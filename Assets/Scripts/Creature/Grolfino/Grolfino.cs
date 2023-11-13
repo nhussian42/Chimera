4b99059d7e74cf8a86bfe2cf2ca79268efa35cc1
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-11-13 00:54:52 -0500

    boss enrages at 50% health

--- Assets/Scripts/Creature/Grolfino/Grolfino.cs
+++ Assets/Scripts/Creature/Grolfino/Grolfino.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa7ee37c178574cfdc01071bf5b291c1fba98bee71cbb0b4b2caab930f5ae017
-size 8797
+oid sha256:44c02e14289b4fc70f5047cc07e194ec8e14d3d8d4b686ea4216b8d9c8f744c4
+size 9341
