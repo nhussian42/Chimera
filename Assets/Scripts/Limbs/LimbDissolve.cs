dafe98e9e2c5ae3783bb5ebebf07b4fe07df3e23
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-11-14 15:32:56 -0500

    wolf arm update

--- Assets/Scripts/Limbs/LimbDissolve.cs
+++ Assets/Scripts/Limbs/LimbDissolve.cs
@@ -2,31 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SkinnedMeshRenderer))]
 public class LimbDissolve : MonoBehaviour
 {
-    float lerpDuration = 3; 
+    [SerializeField] float dissolveDuration = 3; 
     [SerializeField] float startValue = 0; 
     [SerializeField] float endValue = 10; 
     float valueToLerp;
 
     Material mat;
 
-    void Start()
+    private void Start()
     {
         
         mat = GetComponent<SkinnedMeshRenderer>().material;
         StartCoroutine(Lerp());
     }
-    IEnumerator Lerp()
+    private IEnumerator Lerp()
     {
         float timeElapsed = 0;
-        while (timeElapsed < lerpDuration)
+        while (timeElapsed < dissolveDuration)
         {
-            valueToLerp = Mathf.Lerp(startValue, endValue, timeElapsed / lerpDuration);
+            valueToLerp = Mathf.Lerp(startValue, endValue, timeElapsed / dissolveDuration);
             timeElapsed += Time.deltaTime;
             mat.SetFloat("_DissolveHeight", valueToLerp);
             yield return null;
         }
+
         valueToLerp = endValue;
         float temp = startValue;
         startValue = endValue;
@@ -35,4 +37,8 @@ public class LimbDissolve : MonoBehaviour
         StartCoroutine(Lerp());
     }
 
+    private void OnDisable()
+    {
+        
+    }
 }
