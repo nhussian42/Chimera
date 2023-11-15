100d6c6002f1ab37bd2cc0fd964ab849cc2ecbca
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-11-14 22:29:10 -0500

    dissolve material creation part 1

--- Assets/Scripts/Limbs/LimbDissolve.cs
+++ Assets/Scripts/Limbs/LimbDissolve.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:abb86b263a1b4d7b3000a1300872460551b6e600b2a34bd8ab52eb9a397a80a6
-size 1063
+oid sha256:7cee8cd86665551106271e3d6cd1b27a2263c2cc702c5234f85c9fc28f35e4a8
+size 1241
