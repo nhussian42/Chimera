dd49977547ec5e9eb6705ef67f6c49bfbb5cf8af
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-11-15 21:26:33 -0500

    Fixed rhino rotation

--- Assets/Scripts/Creature/Major Creatures/Rhino.cs
+++ Assets/Scripts/Creature/Major Creatures/Rhino.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce351d7604eafadd6f2166b717e030c90b8224551be5ac213bd507029d1244b1
-size 6374
+oid sha256:64934e3bcf345286ea226c37a3d7ed889ecab479071980416b798238ac12a78b
+size 6429
