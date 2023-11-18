6d72c10c9cb7848356d0c8c0b16c6de46c217627
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-11-18 17:55:47 -0500

    Enemy colliders disable on death

--- Assets/Scripts/Creature/Creature.cs
+++ Assets/Scripts/Creature/Creature.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7510d5439a07bd1af3ef37dc92d68078d8e7e613f6e0ecf675c4cc268dbffaed
-size 4471
+oid sha256:64087c8f8e0986315f5b06a21ac91f39acfff97c2029dfdaaa5cb7d8c41fc381
+size 4441
--- Assets/Scripts/Creature/Major Creatures/Crocodile.cs
+++ Assets/Scripts/Creature/Major Creatures/Crocodile.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b905e2515ed53542f3632a98deaccb545fcacd71f9030613b41e855f9ec304ad
-size 6142
+oid sha256:424795cae19834705fee140ea131dbe44cd60b9716035a9d2635ca8c5fbd98cd
+size 5989
