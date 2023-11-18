7a9b06d2019bc5dd06807d55f0b9dd54c884e0e0
Braydon-Leamer <luminati.confirmed@yahoo.com>
2023-11-17 23:21:56 -0500

    Updated Pause Menu and Added Spikes

--- Assets/Scripts/Triggers/HazardScript.cs
+++ Assets/Scripts/Triggers/HazardScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c91f534ae5128cfb830fb92a6b9dc40d0c6c7790ae08585c455ed6ca4095228
-size 1528
+oid sha256:c9abc7b026e08d9b737d7ed6c25f8e039184561ffed7ae303baebc093cfdf222
+size 3012
