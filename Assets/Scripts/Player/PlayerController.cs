10f9e432b31078ffd1f8db46d82ba017df54d02d
amguerrero2 <77288733+amgue-create@users.noreply.github.com>
2023-11-18 16:14:03 -0500

    Bones UI and Persistence
    Added CurrencyManager class and prefab
    Added CurrencyHUD prefab
    Bones Persist

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3123434ca0740cbaaf10d7d4860bcfe80b00a23b66060375e86cdc9281ff890
-size 36949
+oid sha256:288139a9af23b5856fcff35aa14345ac799424b181a1fd05c2af7412151fd607
+size 36981
