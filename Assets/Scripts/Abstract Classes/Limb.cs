2823c1bd6b692fb80115d31afad1fbc4a449d8c2
amguerrero2 <77288733+amgue-create@users.noreply.github.com>
2023-11-17 23:00:56 -0500

    Updated Limb Swap Menu
    Connected sprite and name values on limb scripts to swap menu

--- Assets/Scripts/Abstract Classes/Limb.cs
+++ Assets/Scripts/Abstract Classes/Limb.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e62d88bde48eee189204752adb5acdd3a4f14ca1d0c923100b36adf6a81a2d1f
-size 1676
+oid sha256:f153fca945a4d961c2fe2619a6686dc34daf349f3ce57095b319e3f1657fdd85
+size 1843
--- Assets/Scripts/UI/Screen Space/LimbSwapMenu.cs
+++ Assets/Scripts/UI/Screen Space/LimbSwapMenu.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:deae783379237d7ac76a0c8e79102f45ac0ff9e26e6b1e47be88c9878191cf27
-size 11914
+oid sha256:c329dfa6556f5ba3b9ec5581b61d77e9d96bd38ec68a4d39c1edb9db9e5836ca
+size 12387
