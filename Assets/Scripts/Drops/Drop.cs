c833a107c0a92142a9e5f19309132da0a877a0c6
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-11-18 10:42:43 -0500

    moving scripts around

---
+++ Assets/Scripts/Drops/Drop.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0d0e5676ef94df8ae1a851c12aafcb1df9013b1b3db51ad6f29f1f914e4b5840
+size 754
---
+++ Assets/Scripts/Drops/LimbDrop.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bc4c53d730b0b4fa192f0c5e4c85aa07db146be0fef83593477336f32564be5c
+size 952
