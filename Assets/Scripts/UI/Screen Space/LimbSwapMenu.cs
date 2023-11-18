b511e49f48abd6b91260d17bff49df4a0411fb5f
amguerrero2 <77288733+amgue-create@users.noreply.github.com>
2023-11-18 01:27:37 -0500

    Swap Menu Update
    - Includes sprites and names
    - Pickup indicators for limb pickups
    - I moved the CrocArmDrop prefab from the mammalian folder to the reptilian folder

--- Assets/Scripts/UI/Screen Space/LimbSwapMenu.cs
+++ Assets/Scripts/UI/Screen Space/LimbSwapMenu.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c329dfa6556f5ba3b9ec5581b61d77e9d96bd38ec68a4d39c1edb9db9e5836ca
-size 12387
+oid sha256:6fe7d474f2d190e933238a400a360b48b333dc76a3cf2571b01c99fc9f403be1
+size 14745
