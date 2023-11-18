fe7ca6997cfc1aaf085dc5b189a110cddca02601
Braydon-Leamer <luminati.confirmed@yahoo.com>
2023-11-17 23:52:18 -0500

    Health UI Tracks Leg Health

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:307c6281ce2a541ca8a21ab42e02838a78373b0db6e2c94bc447baa27aeedd10
-size 37333
+oid sha256:f47401ca26e17490d343b9e497d36893a388cfeaa44a0c9f86e53d3d3793d22c
+size 37337
--- Assets/Scripts/UI/Screen Space/LimbHealthBars.cs
+++ Assets/Scripts/UI/Screen Space/LimbHealthBars.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d1d6c2c52802fd6d48de9fe7c64857dccb2b4195275a8e3d87f49f94952baec
-size 3986
+oid sha256:e7e2884ccc03aa9d09912a347bcc3dcdffe2395553580277ccd5eb70c3f2263c
+size 3812
