198d3158738343498caa2e3d88b9bff2d6e5e366
amguerrero2 <77288733+amgue-create@users.noreply.github.com>
2023-11-17 21:09:08 -0500

    Controller support for limb swap menu

--- Assets/Scripts/UI/Screen Space/LimbSwapMenu.cs
+++ Assets/Scripts/UI/Screen Space/LimbSwapMenu.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47f46678f81bad1d61b147efa53d4379241f00906e3c4a92e87a933b2d716102
-size 12028
+oid sha256:56fd0a4bc6f39b447eb17b42258ddb5f82dbacf25af673fd446defdd81136f6c
+size 14085
