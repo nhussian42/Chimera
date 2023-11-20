efab4366ba9f2b160d230e48986f1c16d6c54361
amguerrero2 <77288733+amgue-create@users.noreply.github.com>
2023-11-20 18:26:09 -0500

    Set Up PRS Controller and edited PRS Manager
    PRS = Post Run Summary

--- Assets/Scripts/UI/Screen Space/PostRunSummaryController.cs
+++ Assets/Scripts/UI/Screen Space/PostRunSummaryController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3aefb96a7de97600bba2e607c930ed5196826fc02b2ac34c38aee22d3b19ecc
-size 2969
+oid sha256:9cd88b051d476fe6f70f25ddcb7b0521075daafec9e0a0dc5e75b0ba9ea17fd5
+size 1479
--- Assets/Scripts/UI/Screen Space/TimerDebug.cs
+++ Assets/Scripts/UI/Screen Space/TimerDebug.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:854ec5ca096999c99663dd59d4cc4754f1a42f8e8c5237a9dbc55ef43c9bfc38
-size 872
+oid sha256:c33f29634b455fa3c4f15b8fb318d054b5bcc2248696a20225d35a76ae61c63a
+size 860
