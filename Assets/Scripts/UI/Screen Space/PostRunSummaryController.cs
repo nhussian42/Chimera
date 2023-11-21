194243a6e02f743d2fa8b05db5e5bdafc8b59e80
amguerrero2 <77288733+amgue-create@users.noreply.github.com>
2023-11-20 23:45:10 -0500

    Added PRS UI Prefab + Updated PRS Manager Code

--- Assets/Scripts/UI/Screen Space/PostRunSummaryController.cs
+++ Assets/Scripts/UI/Screen Space/PostRunSummaryController.cs
@@ -2,18 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PostRunSummaryController : MonoBehaviour
 {
     // Private
     private PostRunSummaryManager pRSManager;
+    private int roomsCount;
+    private int timerCount;
+    private int snapshotIndex;
 
     // Exposed
     [SerializeField] MasterTrinketList masterTrinketList;
+    [SerializeField] List<Image> trinketSlots;
 
     // Exposed UI
     [SerializeField] TextMeshProUGUI timerText;
     [SerializeField] TextMeshProUGUI roomsClearedText;
+    [SerializeField] Image head;
+    [SerializeField] Image leftArm;
+    [SerializeField] Image rightArm;
+    [SerializeField] Image legs;
 
     // Start is called before the first frame update
     void Start()
@@ -31,6 +40,13 @@ public class PostRunSummaryController : MonoBehaviour
         // unsubscribe to C# events here
     }
 
+    private void Initialize()
+    {
+        roomsCount = 0;
+        timerCount = 0;
+        snapshotIndex = 0;
+    }
+
     private void Display()
     {
         // Uses coroutines to display information in order
@@ -44,8 +60,12 @@ public class PostRunSummaryController : MonoBehaviour
 
     private IEnumerator PlayRoomsClearedText()
     {
-        // displays and formats rooms cleared text
-        yield return null;
+        // displays and formats 'rooms cleared' text
+        roomsClearedText.text = "Rooms: " + roomsCount.ToString();
+        yield return new WaitForSeconds(0.25f);
+        if (roomsCount < pRSManager.roomsCleared)
+            roomsCount++;
+            StartCoroutine(PlayRoomsClearedText());
     }
 
     private void ReadPlayerTrinketInventory()
@@ -56,8 +76,23 @@ public class PostRunSummaryController : MonoBehaviour
     private IEnumerator StartLimbsTimelapse()
     {
         // gets lists from PostRunSummaryManager to display a snapshot of player's limb build after each room in sequential order
-        yield return null;
-        
+        if (snapshotIndex <= pRSManager.headsInRun.Count)
+            head.sprite = pRSManager.headsInRun[snapshotIndex];
+        if (snapshotIndex <= pRSManager.leftArmsInRun.Count)
+            leftArm.sprite = pRSManager.leftArmsInRun[snapshotIndex];
+        if (snapshotIndex <= pRSManager.rightArmsInRun.Count)
+            rightArm.sprite = pRSManager.rightArmsInRun[snapshotIndex];
+        if (snapshotIndex <= pRSManager.leftArmsInRun.Count)
+            legs.sprite = pRSManager.legsInRun[snapshotIndex];
+        yield return new WaitForSeconds(0.25f);
+        if (snapshotIndex < pRSManager.headsInRun.Count 
+            || snapshotIndex < pRSManager.leftArmsInRun.Count 
+            || snapshotIndex < pRSManager.rightArmsInRun.Count 
+            || snapshotIndex < pRSManager.leftArmsInRun.Count)
+        {
+            snapshotIndex++;
+            StartCoroutine(StartLimbsTimelapse());
+        }
     }
 
     //Retry function?
