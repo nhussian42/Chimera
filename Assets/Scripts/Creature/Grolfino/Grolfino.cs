9a13da9bc6be01c98f99b7dc16900a4f2ce117f5
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-11-21 03:20:07 -0500

    Adjusted boss attack order

--- Assets/Scripts/Creature/Grolfino/Grolfino.cs
+++ Assets/Scripts/Creature/Grolfino/Grolfino.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44c02e14289b4fc70f5047cc07e194ec8e14d3d8d4b686ea4216b8d9c8f744c4
-size 9341
+oid sha256:4c15a3106a915891743b828432dfee1077b3e6f21c5a784acc9eb8258b930453
+size 9227
