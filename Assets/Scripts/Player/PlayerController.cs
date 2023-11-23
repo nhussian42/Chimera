8a7a8a3262e4ac5794de86bbd7ca1d52fa4dcf3c
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-11-22 19:36:40 -0500

    walk

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87ebcd6480d448c12d7b61f8f45339940ef39013cfe005c2d798428a981c3ce5
-size 38340
+oid sha256:22641dfe0ba0eb20420de92f643d7714d17f9898f2bd5e94796f77760e5fd6ff
+size 37739
