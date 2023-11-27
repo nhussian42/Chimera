2ff8e43728eb556e615af585f553016811c60827
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-11-26 20:36:34 -0500

    Changed tortoise numbers

--- Assets/Scripts/Creature/Minor Creatures/Tortoise/Tortoise.cs
+++ Assets/Scripts/Creature/Minor Creatures/Tortoise/Tortoise.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da543e0a6d4f43f82ae22c2c72c4a7fc3d1ca3c9e8cc3c0aef83b520c95bf8af
-size 1929
+oid sha256:0e9dac6c894ff423c6316e5862cf334b01085dc115527e8ad278484d6a90bb6d
+size 1932
