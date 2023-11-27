fadbc03c952075260b1f8fc62da8bc802efe3294
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-11-26 20:25:49 -0500

    Updated colliders on minor creatures

--- Assets/Scripts/Creature/Major Creatures/Gecko/Gecko.cs
+++ Assets/Scripts/Creature/Major Creatures/Gecko/Gecko.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8530c3b5125a1c39da8c95ef4e77a97d32681e83f855a03a0a64921350df9c3b
-size 6167
+oid sha256:1bee8f515f5b90f250677d3c3c576cf1675c90957c5d357d5a457edab7d42aca
+size 6162
--- Assets/Scripts/Creature/Minor Creatures/Tortoise/Tortoise.cs
+++ Assets/Scripts/Creature/Minor Creatures/Tortoise/Tortoise.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a82e4a56f97094c8da177325b1bcb32349fe8f70d510febf2234531bfb131fc4
-size 1931
+oid sha256:da543e0a6d4f43f82ae22c2c72c4a7fc3d1ca3c9e8cc3c0aef83b520c95bf8af
+size 1929
