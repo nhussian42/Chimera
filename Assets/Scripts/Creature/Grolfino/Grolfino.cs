8557ce635404532f681d3982755391e55f7a0ff5
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-11-27 04:47:44 -0500

    initial boss anim implementation

--- Assets/Scripts/Creature/Grolfino/Grolfino.cs
+++ Assets/Scripts/Creature/Grolfino/Grolfino.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5a55d1f916ca49384e9a43b739aa01366738011a06ee8ec2f56ecd16465b6cf
-size 10338
+oid sha256:69130d6c7580d420e313c8fc77806115af94ca21fbe9564378ddfc51495c366d
+size 10521
---
+++ Assets/Scripts/Creature/Grolfino/GrolfinoAnimationEvents.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:faad3b3280af360a614b88ee9ffdfe093228b7dde411638ff7c0ed7c24643216
+size 1909
