f4aaae3c1cb4752914de6fb9a5e168000a173667
Braydon-Leamer <luminati.confirmed@yahoo.com>
2023-11-28 16:44:02 -0500

    Fix limbdrop

--- Assets/Scripts/Drops/LimbDrop.cs
+++ Assets/Scripts/Drops/LimbDrop.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f860811bc34a6edab484db8900ab126ee5edfce557af665c4acb5c6f108b07d
-size 1623
+oid sha256:9313c69fa3b240ff9bfa7e515ff589a43125d5edf3cdb4aea36f857dbf9826fe
+size 1614
