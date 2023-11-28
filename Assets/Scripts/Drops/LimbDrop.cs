4319ec682beeb614614c3d575cdfe569444f7e57
Braydon-Leamer <luminati.confirmed@yahoo.com>
2023-11-28 16:44:02 -0500

    Fix limbdrop

--- Assets/Scripts/Drops/LimbDrop.cs
+++ Assets/Scripts/Drops/LimbDrop.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class LimbDrop : MonoBehaviour
+public class LimbDrop : Drop
 {
     
     [SerializeField] protected Weight weight;
