e58781b9e3f2b6a0c6c6f1f0139bca99e4922c51
GabrielFernandez14 <79731548+GabrielFernandez14@users.noreply.github.com>
2023-11-30 11:53:36 -0500

    Completed CameraZoom script and started work on CutsceneController

---
+++ Assets/Scripts/Systems/Camera/CutsceneController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c2d1ce2ec41e28d1e6cc99189d55a9c1e28e6192565de3ab1144ade5f8a3fed9
+size 1107
---
+++ Assets/Scripts/Triggers/BossCutsceneTrigger.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3d15eda79d1d1a6432d0780c91bd754b1926a4dd19083292546f140677fefdf1
+size 580
