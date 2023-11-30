4f56234a0f53753ddb65400ae44d1eef4e3c330d
Nick <56888291+nhussian42@users.noreply.github.com>
2023-11-30 12:53:19 -0500

    getting rid of debug log

--- Assets/Scripts/UI/Screen Space/SliderHighlight.cs
+++ Assets/Scripts/UI/Screen Space/SliderHighlight.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58c0bfae90d4e44abd358fcf4b7c357b636b247bac60a33c8a7c4bb73d15adb1
-size 826
+oid sha256:29240365286e65769dab6021cae9a94f50d135acabcc8b52af45704d0baf2d24
+size 830
