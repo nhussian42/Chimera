23976252d84b9acbd0903811a4a6f4a86f753d66
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-12-01 16:14:02 -0500

    boss correctly spawns and door opens when it dies

--- Assets/Scripts/Triggers/Boss1CutsceneTrigger.cs
+++ Assets/Scripts/Triggers/Boss1CutsceneTrigger.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ab10df4a1d34433d57252a0903a0d30f016278d6986996bf8833cedffe36db4
-size 1620
+oid sha256:be261a7f63d5f06fe047ea3ad8af7b58bbd00845a70d46654840a0a7ce711058
+size 1867
