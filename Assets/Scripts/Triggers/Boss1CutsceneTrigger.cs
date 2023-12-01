a24ad9e84b71b15c485e64753e19076b490c4dbd
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-11-30 20:23:30 -0500

    initial boss trigger stuff

--- Assets/Scripts/Triggers/Boss1CutsceneTrigger.cs
+++ Assets/Scripts/Triggers/Boss1CutsceneTrigger.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class Boss1CutsceneTrigger : MonoBehaviour
 {
     private bool _triggered;
+    [SerializeField] private GameObject boss;
+    [SerializeField] private GameObject gate;
 
     // private Collider cutsceneTrigger;
 
@@ -23,6 +26,32 @@ public class Boss1CutsceneTrigger : MonoBehaviour
         {
             _triggered = true;
             Boss1Cutscene?.Invoke();
+            StartCoroutine(CloseGate());
+            Invoke("SpawnBoss", 3f);
+            PlayerController.Instance.DisableAllDefaultControls();
+            Invoke("ResumePlayerControls", 4f);
         }
+    }
+
+    private void SpawnBoss()
+    {
+        boss.SetActive(true);
+    }
+
+    private IEnumerator CloseGate()
+    {
+        float timer = 0;
+        while (timer < 0.5)
+        {
+            timer += Time.deltaTime;
+            gate.transform.position = new Vector3(gate.transform.position.x, Mathf.Lerp(8, 0, timer / 0.5f), gate.transform.position.z);
+            yield return null;
+        }
+        yield return null;
+    }
+
+    private void ResumePlayerControls()
+    {
+        PlayerController.Instance.EnableAllDefaultControls();
     }
 }
