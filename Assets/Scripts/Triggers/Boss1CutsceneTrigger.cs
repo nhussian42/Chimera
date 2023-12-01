86b7a1dc2659c31a4b9ec2d32a45d9a83dc4f645
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-11-30 20:23:30 -0500

    initial boss trigger stuff

--- Assets/Scripts/Triggers/Boss1CutsceneTrigger.cs
+++ Assets/Scripts/Triggers/Boss1CutsceneTrigger.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8bd3800ed42301cfd2a872ea248f4eadeb4d86bfbb63636dc494732777687b36
-size 584
+oid sha256:9857bf23adcbed79e92e53d49d30ff3ec562d068660108f5fa2817eec8f618d1
+size 1434
