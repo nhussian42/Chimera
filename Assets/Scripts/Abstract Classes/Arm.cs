8ca0e90d0415325b2c731d800bafb8e2fc36ab84
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-12-02 16:26:32 -0500

    fixing attacks

--- Assets/Scripts/Abstract Classes/Arm.cs
+++ Assets/Scripts/Abstract Classes/Arm.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc6e42115e89a2ba011be460af66952d57f0f81e2c05c486bb9c5e2a58e8687f
-size 6656
+oid sha256:5003c458712c4ba8466d31f8e774bacf899e8657fe99c035e9cd9e286eb91edb
+size 6674
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae0b8e6f69280edc3a73ec47d9ec1f10352e07b3abdc774a7291223a2ec07950
-size 37478
+oid sha256:57940ed34b27c5459f52ab66bf0adc51c372a9300176ba058df2c5f4bdb46806
+size 37534
