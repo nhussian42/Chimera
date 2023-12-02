f989f342df0d883ee25636b48e64b78432bac12a
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-12-01 21:39:34 -0500

    arm updates with rotation

--- Assets/Scripts/Abstract Classes/Arm.cs
+++ Assets/Scripts/Abstract Classes/Arm.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8ae98cd5cdd1dde9b7ceb71fd5bfd948985e7193c238be4897620645548f9a8
-size 6260
+oid sha256:b6867d899be140e133c7ca21f93de6ff57b743033f202057869662ad3c89545b
+size 6585
--- Assets/Scripts/Limbs/Arms/AttackRange.cs
+++ Assets/Scripts/Limbs/Arms/AttackRange.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:989230af9a6fe2679dbadd733a2b603ec7e1c5750d4dbeeb12a02f953d4f901d
-size 1195
+oid sha256:c07f7e4e1eeb9c92614b0c3629577fda6b363a77c05a8c102713adf97dfbbb4b
+size 1754
