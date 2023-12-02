3d36f281ca434b5bc7911253039283c534a1f9a1
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-12-01 20:00:47 -0500

    croc bite vfx

--- Assets/Scripts/Creature/Major Creatures/Croc/Crocodile.cs
+++ Assets/Scripts/Creature/Major Creatures/Croc/Crocodile.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ee4550b21a4e4f108318b8b0799bd0aee66de6686d1f241a22470d349c76959
-size 6765
+oid sha256:4f4c1d77d9d8085627e8b5a7a6e4198c8936328cc2ce8c8734ecfc81b5d376b3
+size 6593
--- Assets/Scripts/Creature/Major Creatures/Croc/CrocodileAnimationEvents.cs
+++ Assets/Scripts/Creature/Major Creatures/Croc/CrocodileAnimationEvents.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51f3513062d05f24943850f1e5a0de60ab8c6c0da74f17d2cf0b0f9e49ae7456
-size 839
+oid sha256:06c72fe1d3814ca850ae03062bf28a31f6f3458b090a9f75311b140a688a00d1
+size 1137
