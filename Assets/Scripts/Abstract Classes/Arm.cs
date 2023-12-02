abe60c01ca09ae604860d8b94e50e3671c7e1496
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-12-01 22:27:06 -0500

    rhino/croc attack vfx

--- Assets/Scripts/Abstract Classes/Arm.cs
+++ Assets/Scripts/Abstract Classes/Arm.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6867d899be140e133c7ca21f93de6ff57b743033f202057869662ad3c89545b
-size 6585
+oid sha256:cc6e42115e89a2ba011be460af66952d57f0f81e2c05c486bb9c5e2a58e8687f
+size 6656
--- Assets/Scripts/Limbs/Arms/AttackRange.cs
+++ Assets/Scripts/Limbs/Arms/AttackRange.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c07f7e4e1eeb9c92614b0c3629577fda6b363a77c05a8c102713adf97dfbbb4b
-size 1754
+oid sha256:824a7f8fd6fd49239698d1684ee35626360d4e343cfca37b895095a63da8f3ed
+size 1841
