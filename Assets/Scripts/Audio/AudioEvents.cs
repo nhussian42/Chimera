27f2a3611c03ba216c5db3203f2f0b7fe79d7b48
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-12-03 09:41:48 -0500

    added luzzy's sounds to FMOD bank

--- Assets/Scripts/Audio/AudioEvents.cs
+++ Assets/Scripts/Audio/AudioEvents.cs
@@ -17,14 +17,26 @@ public class AudioEvents : Singleton<AudioEvents>
     [field: SerializeField] public EventReference OnGameQuit { get; private set; }
     [field: SerializeField] public EventReference OnMenuButtonSelected { get; private set; }
     [field: SerializeField] public EventReference OnMenuButtonHovered { get; private set; }
+    [field: SerializeField] public EventReference OnPurchaseItems { get; private set; }
+    [field: SerializeField] public EventReference OnScrapLimb { get; private set; }
 
     [field: Header("Player SFX Events")]
-    [field: SerializeField] public EventReference OnPlayerWalk { get; private set; }
+
+    [field: Header("Player Attacks")]
     [field: SerializeField] public EventReference OnPlayerBaseAttack { get; private set; }
     [field: SerializeField] public EventReference OnPlayerWolfAttack { get; private set; }
     [field: SerializeField] public EventReference OnPlayerCrocAttack { get; private set; }
     [field: SerializeField] public EventReference OnPlayerRhinoAttack { get; private set; }
     [field: SerializeField] public EventReference OnPlayerGeckoAttack { get; private set; }
+
+    [field: Header("Player Movement")]
+    [field: SerializeField] public EventReference OnPlayerWalk { get; private set; }
+    [field: SerializeField] public EventReference OnPlayerBaseDash { get; private set; }
+    [field: SerializeField] public EventReference OnPlayerPounce { get; private set; }
+    [field: SerializeField] public EventReference OnPlayerBurrow { get; private set; }
+    
+    [field: Header("Player Status")]
+    [field: SerializeField] public EventReference OnPlayerStun { get; private set; }
 
     [field: Header("Impact SFX Events")]
     [field: SerializeField] public EventReference OnPlayerDamagedSmall { get; private set; }
@@ -34,9 +46,11 @@ public class AudioEvents : Singleton<AudioEvents>
     [field: SerializeField] public EventReference OnPlayerHitConnected { get; private set; }
 
     [field: Header("Creature SFX Events")]
+
     [field: Header("All Creatures")]
     [field: SerializeField] public EventReference OnCreatureDamaged { get; private set; } // Placeholder for all creatures take damage events
-    [field: SerializeField] public EventReference OnBossDefeated { get; private set; }
+
+    [field: Header("Minor Enemies")]
 
     [field: Header("Hedgehog")]
     [field: SerializeField] public EventReference OnHedgehogAttack { get; private set; }
@@ -47,7 +61,9 @@ public class AudioEvents : Singleton<AudioEvents>
     [field: SerializeField] public EventReference OnTortoiseAttack { get; private set; }
     [field: SerializeField] public EventReference OnTortoiseDamaged { get; private set; }
     [field: SerializeField] public EventReference OnTortoiseDeath { get; private set; }
-    
+
+    [field: Header("Major Enemies")]
+
     [field: Header("Wolf")]
     [field: SerializeField] public EventReference OnWolfAttack { get; private set; }
     [field: SerializeField] public EventReference OnWolfDamaged { get; private set; }
@@ -60,7 +76,6 @@ public class AudioEvents : Singleton<AudioEvents>
     [field: SerializeField] public EventReference OnCrocDamaged { get; private set; }
     [field: SerializeField] public EventReference OnCrocDeath { get; private set; }
     
-
     [field: Header("Rhino")]
     [field: SerializeField] public EventReference OnRhinoSlam { get; private set; }
     [field: SerializeField] public EventReference OnRhinoCharge { get; private set; }
@@ -73,9 +88,15 @@ public class AudioEvents : Singleton<AudioEvents>
     [field: SerializeField] public EventReference OnGeckoDamaged { get; private set; }
     [field: SerializeField] public EventReference OnGeckoDeath { get; private set; }
 
-    [field: Header("Boss1")]
-    [field: SerializeField] public EventReference OnBoss1Attack { get; private set; }
-    [field: SerializeField] public EventReference OnBoss1Death { get; private set; }
+    [field: Header("Bosses")]
+    [field: Header("Centipede")]
+    [field: SerializeField] public EventReference OnCentipedeSpawn { get; private set; }
+    [field: SerializeField] public EventReference OnCentipedeBurrow { get; private set; }
+    [field: SerializeField] public EventReference OnCentipedeSwipeAttack { get; private set; }
+    [field: SerializeField] public EventReference OnCentipedeScreamAttack { get; private set; }
+    [field: SerializeField] public EventReference OnCentipedeStompScreamAttack { get; private set; }
+    [field: SerializeField] public EventReference OnCentipedeSpreadAttack { get; private set; }
+    [field: SerializeField] public EventReference OnCentipedeDefeated { get; private set; }
 
     [field: Header("Room SFX Events")]
     [field: SerializeField] public EventReference OnRoomCleared { get; private set; }
