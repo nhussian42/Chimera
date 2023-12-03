20bf4087a281f3a85654d5a967f6bf2db43cb210
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-12-02 20:49:41 -0500

    room gen updates

--- Assets/Scripts/Systems/Managers/FloorManager.cs
+++ Assets/Scripts/Systems/Managers/FloorManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1cc616ef5560d40b4f255c886c0c8317d4395be68a06598f9369844029dc8d27
-size 7704
+oid sha256:73eec835b64d1459bbea8c9b3bb2b3d1ad520b48211e5d15d1ff20c652249d77
+size 8158
