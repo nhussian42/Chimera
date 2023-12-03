cd0125602a54b4dac1421e2f8cbd43d3fac329a1
Braydon-Leamer <luminati.confirmed@yahoo.com>
2023-12-03 02:34:08 -0500

    body shop updates

--- Assets/Scripts/UI/World Space/BodyShopMenu.cs
+++ Assets/Scripts/UI/World Space/BodyShopMenu.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bbda3b46dfbd30110a3ca07870131ccb04a9858eee4267138bd9564f10be49ca
-size 7865
+oid sha256:c6ef22bd0990e6e3cf1782a2f6cc2c4529965de1220a8f27cb229e07e49584b8
+size 9747
