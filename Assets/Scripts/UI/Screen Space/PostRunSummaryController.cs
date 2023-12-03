36e0aef0e41d65eb9b5df36abf63f16648549954
Braydon-Leamer <luminati.confirmed@yahoo.com>
2023-12-03 14:36:01 -0500

    Editing Equip Canvas

--- Assets/Scripts/UI/Screen Space/PostRunSummaryController.cs
+++ Assets/Scripts/UI/Screen Space/PostRunSummaryController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03f42522645bba4027037cec9f9e0d49f323bb441447e60bdf3ce2a41e81a6ce
-size 5637
+oid sha256:2b83dd70497d2f5f7c0db7c682d7403a3f1233cf2f7644b1b613996b2e43c361
+size 5633
--- Assets/Scripts/UI/World Space/BodyShopMenu.cs
+++ Assets/Scripts/UI/World Space/BodyShopMenu.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf4f64d56fd0ef3618009ffb9a128921085742cab6218d4e02b644137e3ed125
-size 9737
+oid sha256:2cea4794cabd1b6922249be0fb3338fc7a0824a9d90c1a178d4e1cad38dbdd37
+size 9812
