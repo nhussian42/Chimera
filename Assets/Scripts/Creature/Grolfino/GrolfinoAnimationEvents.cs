bf1eed09b24d54d04164a310f54d302353868962
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-12-03 22:10:16 -0500

    more boss vfx stuff

--- Assets/Scripts/Creature/Grolfino/GrolfinoAnimationEvents.cs
+++ Assets/Scripts/Creature/Grolfino/GrolfinoAnimationEvents.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92dd3ffcc9f7869c33e1c11ec2668c13b855382fbaef2010b7fda1412790b82d
-size 2317
+oid sha256:8df7f627230771e936ab806175ad0a1898a05413d016b9227fdef3a3aa71d3c2
+size 4469
