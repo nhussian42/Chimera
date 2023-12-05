df1130010f13e1e72d0a80dc69cc21188b1f68c9
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-12-04 22:53:23 -0500

    Update BodyShopMenu.cs

--- Assets/Scripts/UI/World Space/BodyShopMenu.cs
+++ Assets/Scripts/UI/World Space/BodyShopMenu.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b573e28e6545705d9f5b2e1dd4244651179eb09a1f40b467875364cdcacc39c7
-size 9944
+oid sha256:05151cd2a07c45cbb3a64f853fdec5983bb4c4950ab811f84bd66d4445f9784d
+size 9942
