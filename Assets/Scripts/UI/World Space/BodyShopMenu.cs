f8abedcec6c56aa446df301170545c621fb83d32
Braydon-Leamer <luminati.confirmed@yahoo.com>
2023-12-04 22:26:52 -0500

    touchups

--- Assets/Scripts/UI/World Space/BodyShopMenu.cs
+++ Assets/Scripts/UI/World Space/BodyShopMenu.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2cea4794cabd1b6922249be0fb3338fc7a0824a9d90c1a178d4e1cad38dbdd37
-size 9812
+oid sha256:84f5cd61084af1a2c674697ad28df61ff7d12a767c86eec0d1de8f894659e7f8
+size 9932
