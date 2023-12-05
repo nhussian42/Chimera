edb52faab4dd10f5d8694441e49516d46c4f4c04
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-12-04 21:50:32 -0500

    menu updates & boss healthbar changes

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c315b99cf493b96ab3bb4764d22dae5e199384391875f40c271c18fdfe4aa006
-size 39380
+oid sha256:020b8602257b712925e36e3d4b61dbd3f6e0ca73b64fbade5593fa33de216744
+size 39862
